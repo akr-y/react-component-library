5f0d9049f3045841790e422e175062fae6d38e68
Akira Yamamoto <yo@akira.run>
2020-02-23 23:10:25 +0900

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Button } from './components/Button';
 import { TextField } from './components/Field/TextField';
+import { Stack } from './components/Stack';
 
-export { Button, TextField };
+export { Button, TextField, Stack };
